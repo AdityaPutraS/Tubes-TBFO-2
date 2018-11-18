5a3b536a6e0f403c441c99aee2f5bd1fa2ef13c1
Aditya Putra Santosa <adityaputra159@gmail.com>
2018-11-18 20:22:34 +0700

    Add Trigono & Inverse Trigono (iseng) & BugFix
    Bug :
     - 5^-2 bisa, harusnya nggak, udah di fix
    
    Fitur baru :
     - sin(...), cos(...), tan(...), asin(...), acos(...), atan(...)

--- Riset CFG/mainDank.c
+++ Riset CFG/mainDank.c
@@ -10,6 +10,7 @@ void funcTerm1(char s[], int *letak, boolean *berhasil, boolean *mathError, long
 void funcTerm2(char s[], int *letak, boolean *berhasil, boolean *mathError, long double complex *hasilHitung);
 void funcFactor1(char s[], int *letak, boolean *berhasil, boolean *mathError, long double complex *hasilHitung);
 void funcFactor2(char s[], int *letak, boolean *berhasil, boolean *mathError, long double complex *hasilHitung);
+void funcTrigonometry(char s[], int *letak, boolean *berhasil, boolean *mathError, long double complex *hasilHitung);
 void funcComplex(char s[], int *letak, boolean *berhasil, boolean *mathError, long double complex *hasilHitung);
 void funcBilangan(char s[], int *letak, boolean *berhasil, boolean *mathError, long double complex *hasilHitung);
 void funcUnsigned(char s[], int *letak, boolean *berhasil, boolean *mathError, long int *hasilHitung);
@@ -169,69 +170,41 @@ void funcTerm2(char s[], int *letak, boolean *berhasil, boolean *mathError, long
 
 void funcFactor1(char s[], int *letak, boolean *berhasil, boolean *mathError, long double complex *hasilHitung)
 {
-    // F -> CF' | (E)F'
+    // F -> MF'
     int letakAwal = *letak;
     char c = s[*letak];
-    if (c == '(')
+    // MF'
+    long double complex temp = 0;
+    funcTrigonometry(s, letak, berhasil, mathError, &temp);
+    if (*berhasil)
     {
-        // (E)F'
-        *letak += 1;
-        long double complex temp = 0;
-        funcExpression1(s, letak, berhasil, mathError, &temp);
-        if (*berhasil)
-        {
-            *letak += 1;
-            *hasilHitung = temp;
-            funcFactor2(s, letak, berhasil, mathError, hasilHitung);
-        }
-        else
-        {
-            *letak = letakAwal;
-        }
+        *hasilHitung = temp;
+        funcFactor2(s, letak, berhasil, mathError, hasilHitung);
     }
     else
     {
-        // BF'
-        long double complex temp = 0;
-        funcComplex(s, letak, berhasil, mathError, &temp);
-        if (*berhasil)
-        {
-            *hasilHitung = temp;
-            funcFactor2(s, letak, berhasil, mathError, hasilHitung);
-        }
-        else
-        {
-            *letak = letakAwal;
-        }
+        *letak = letakAwal;
     }
 }
 
 void funcFactor2(char s[], int *letak, boolean *berhasil, boolean *mathError, long double complex *hasilHitung)
 {
-    // F'-> ^(E)F' | ^CF' | epsilon
+    // F'-> ^MF' | epsilon
     int letakAwal = *letak;
     char c = s[*letak];
     if (c == '^')
     {
         char c2 = s[*letak + 1];
-        if (c2 == '(')
+        // ^MF'
+        long double complex temp = 0;
+        *letak += 1;
+        funcTrigonometry(s, letak, berhasil, mathError, &temp);
+        if (*berhasil)
         {
-            // ^(E)F'
-            long double complex temp = 0;
-            *letak += 2;
-            funcExpression1(s, letak, berhasil, mathError, &temp);
+            funcFactor2(s, letak, berhasil, mathError, &temp);
             if (*berhasil)
             {
-                *letak += 1;
-                funcFactor2(s, letak, berhasil, mathError, &temp);
-                if (*berhasil)
-                {
-                    *hasilHitung = cpowl(*hasilHitung, temp);
-                }
-                else
-                {
-                    *letak = letakAwal;
-                }
+                *hasilHitung = cpowl(*hasilHitung, temp);
             }
             else
             {
@@ -240,32 +213,175 @@ void funcFactor2(char s[], int *letak, boolean *berhasil, boolean *mathError, lo
         }
         else
         {
-            // ^CF'
-            long double complex temp = 0;
-            *letak += 1;
-            funcComplex(s, letak, berhasil, mathError, &temp);
-            if (*berhasil)
-            {
-                funcFactor2(s, letak, berhasil, mathError, &temp);
-                if (*berhasil)
-                {
-                    *hasilHitung = cpowl(*hasilHitung, temp);
-                }
-                else
-                {
-                    *letak = letakAwal;
-                }
-            }
-            else
-            {
-                *letak = letakAwal;
-            }
+            *letak = letakAwal;
         }
     }
     else
     {
         // epsilon
         *berhasil = true;
+    }
+}
+
+void funcTrigonometry(char s[], int *letak, boolean *berhasil, boolean *mathError, long double complex *hasilHitung)
+{
+    // M -> sin(E) | cos(E) | tan(E) |
+    //      asin(E) | acos(E) | atan(E) | (E) | C
+
+    int letakAwal = *letak;
+    char c = s[*letak];
+    if (c == '(')
+    {
+        // (E)
+        *letak += 1;
+        long double complex temp = 0;
+        funcExpression1(s, letak, berhasil, mathError, &temp);
+        if (*berhasil)
+        {
+            *hasilHitung = temp;
+            *letak += 1;
+        }
+        else
+        {
+            *letak = letakAwal;
+        }
+    }
+    else
+    {
+        //Cek C
+        long double complex temp = 0;
+        funcComplex(s, letak, berhasil, mathError, &temp);
+        if (*berhasil)
+        {
+            *hasilHitung = temp;
+        }
+        else
+        {
+            //Cek apakah panjangnya masih muat buat nulis sin / cos / tan
+            if (strlen(s) - *letak >= 3)
+            {
+                char c2 = s[*letak + 1];
+                char c3 = s[*letak + 2];
+                if (c == 's' && c2 == 'i' && c3 == 'n')
+                {
+                    // sin(E)
+                    *letak += 4;
+                    long double complex tempTrigono = 0;
+                    funcExpression1(s, letak, berhasil, mathError, &tempTrigono);
+                    if (*berhasil)
+                    {
+                        *hasilHitung = csinl(tempTrigono);
+                        *letak += 1;
+                    }
+                    else
+                    {
+                        *letak = letakAwal;
+                    }
+                }
+                else if (c == 'c' && c2 == 'o' && c3 == 's')
+                {
+                    // cos(E)
+                    *letak += 4;
+                    long double complex tempTrigono = 0;
+                    funcExpression1(s, letak, berhasil, mathError, &tempTrigono);
+                    if (*berhasil)
+                    {
+                        *hasilHitung = ccosl(tempTrigono);
+                        *letak += 1;
+                    }
+                    else
+                    {
+                        *letak = letakAwal;
+                    }
+                }
+                else if (c == 't' && c2 == 'a' && c3 == 'n')
+                {
+                    // tan(E)
+                    *letak += 4;
+                    long double complex tempTrigono = 0;
+                    funcExpression1(s, letak, berhasil, mathError, &tempTrigono);
+                    if (*berhasil)
+                    {
+                        *hasilHitung = ctanl(tempTrigono);
+                        *letak += 1;
+                    }
+                    else
+                    {
+                        *letak = letakAwal;
+                    }
+                }
+                else
+                {
+                    if (strlen(s) - *letak >= 4)
+                    {
+                        //Cek apakah masih muat buat nampung asin / acos / atan
+                        char c2 = s[*letak + 1];
+                        char c3 = s[*letak + 2];
+                        char c4 = s[*letak + 3];
+                        if (c == 'a' && c2 == 's' && c3 == 'i' && c4 == 'n')
+                        {
+                            // asin(E)
+                            *letak += 5;
+                            long double complex tempTrigono = 0;
+                            funcExpression1(s, letak, berhasil, mathError, &tempTrigono);
+                            if (*berhasil)
+                            {
+                                *hasilHitung = casinl(tempTrigono);
+                                *letak += 1;
+                            }
+                            else
+                            {
+                                *letak = letakAwal;
+                            }
+                        }
+                        else if (c == 'a' && c2 == 'c' && c3 == 'o' && c4 == 's')
+                        {
+                            // acos(E)
+                            *letak += 5;
+                            long double complex tempTrigono = 0;
+                            funcExpression1(s, letak, berhasil, mathError, &tempTrigono);
+                            if (*berhasil)
+                            {
+                                *hasilHitung = cacosl(tempTrigono);
+                                *letak += 1;
+                            }
+                            else
+                            {
+                                *letak = letakAwal;
+                            }
+                        }
+                        else if (c == 'a' && c2 == 't' && c3 == 'a' && c4 == 'n')
+                        {
+                            // atan(E)
+                            *letak += 5;
+                            long double complex tempTrigono = 0;
+                            funcExpression1(s, letak, berhasil, mathError, &tempTrigono);
+                            if (*berhasil)
+                            {
+                                *hasilHitung = catanl(tempTrigono);
+                                *letak += 1;
+                            }
+                            else
+                            {
+                                *letak = letakAwal;
+                            }
+                        }
+                        else
+                        {
+                            *letak = letakAwal;
+                        }
+                    }
+                    else
+                    {
+                        *letak = letakAwal;
+                    }
+                }
+            }
+            else
+            {
+                *letak = letakAwal;
+            }
+        }
     }
 }
 
@@ -277,6 +393,7 @@ void funcComplex(char s[], int *letak, boolean *berhasil, boolean *mathError, lo
     char cekI = s[*letak];
     if (cekI == 'i')
     {
+        *berhasil = true;
         *letak += 1;
         *hasilHitung = I;
     }
